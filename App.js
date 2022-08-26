96a3e54d91abcb29a3589fd96078b7afdde7b83c
lukmanhossain <lukman.webdev@gmail.com>
2022-08-26 08:55:39 +0600

    home, login, signup, create, update route set and app.js file setup

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
+import * as React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./src/screens/home";
+import Login from "./src/screens/login";
+import Signup from "./src/screens/signup";
+import Create from "./src/screens/create";
+import Update from "./src/screens/update";
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const user = false; // Not authenticated
   return (
-    <View style={styles.container}>
-      <Text>First App</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      {user ? (
+          <>
+            <Stack.Screen name="Home" component={Home}>
+            </Stack.Screen>
+            <Stack.Screen name="Create" component={Create}>
+            </Stack.Screen>
+            <Stack.Screen name="Update" component={Update} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen
+              name="Login"
+              component={Login}
+            />
+            <Stack.Screen name="Signup" component={Signup} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
